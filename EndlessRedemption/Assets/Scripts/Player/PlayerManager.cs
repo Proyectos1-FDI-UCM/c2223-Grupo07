8e389aea0a9dea8bde7ca08aaf443fd35684411e
SergioPerezRobledano <sepere12@ucm.es>
2023-02-15 10:54:12 +0100

    Update PlayerManager.cs

--- EndlessRedemption/Assets/Scripts/Player/PlayerManager.cs
+++ EndlessRedemption/Assets/Scripts/Player/PlayerManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    private Transform _myTransform;
+    public float _elapsedTime;
+    public float _timeInvulnerable;
+    private bool _invulnerable = false;
+
+    #region methods
+
+    public void Invulnerable()
+    {
+        _invulnerable = true;
+    }
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _myTransform= transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_invulnerable)
+        {
+            gameObject.layer = 8;
+            _elapsedTime += Time.deltaTime;
+            if(_elapsedTime > _timeInvulnerable)
+            {
+                gameObject.layer = 7;
+            }
+        }
     }
 }

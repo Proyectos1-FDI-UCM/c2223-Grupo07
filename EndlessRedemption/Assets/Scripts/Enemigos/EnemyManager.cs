a4fa99bb654d9b3c6292a8bb93e673f06504b199
javierbravoperucho <javierbravoperucho@gmail.com>
2023-02-21 22:24:47 +0100

    Animacion Enemigo, no terminada

--- EndlessRedemption/Assets/Scripts/Enemigos/EnemyManager.cs
+++ EndlessRedemption/Assets/Scripts/Enemigos/EnemyManager.cs
@@ -10,6 +10,7 @@ public class EnemyManager : MonoBehaviour
     private EnemyDetectionComponent _myEnemyDetection;
     private LateralMovement _myLateralMovement;
     private CameraComponent _cameraComponent;
+    private Animator _animator;
 
     static private EnemyManager _instance;
     static public EnemyManager Instance { get { return _instance; } }
@@ -20,6 +21,24 @@ public class EnemyManager : MonoBehaviour
     public float _detectionDistance = 5f; //Distancia a la que detecta al jugador
     public float _appearingDistance; //Distancia a la que aparece del suelo
     private int aparecido = 0;  //Para que solo aparezca una vez
+    public bool _onAppearing = false;
+    private float _elapsedTime = 0f;
+    private float _appearingTime = 1f;
+    #endregion
+
+    #region methods 
+
+    public void Appearing()
+    {
+        _onAppearing=true;
+        _animator.SetBool("Appearing", _onAppearing);
+    }
+
+    public void EndOfAppearing()
+    {
+        _onAppearing = false;
+        _animator.SetBool("Appearing", _onAppearing);
+    }
     #endregion
 
     void Awake()
@@ -36,17 +55,25 @@ public class EnemyManager : MonoBehaviour
         _myEnemyDetection.enabled = false;
         _myLateralMovement.enabled = false;
         _playerTransform = PlayerManager.Instance.transform;
+        GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Mathf.Abs(_playerTransform.position.x - transform.position.x) < _appearingDistance && Mathf.Abs(_playerTransform.position.x - transform.position.x) > _detectionDistance)
+
+        if (Mathf.Abs(_playerTransform.position.x - transform.position.x) < _appearingDistance && Mathf.Abs(_playerTransform.position.x - transform.position.x) > _detectionDistance)
         {
+            GetComponent<Renderer>().enabled = true;
+            Appearing();
+            _elapsedTime += Time.deltaTime;
+            if (_elapsedTime > _appearingTime)
+            {
+                EndOfAppearing();
+            }
             _myLateralMovement.enabled = true;
             aparecido++;
         }
-
         if (Mathf.Abs(_playerTransform.position.x - transform.position.x) < _detectionDistance)
         {
             _myEnemyDetection.enabled = true;

ef8eeb5aad4e19a00076d54525822610e3bc297d
SergioPerez1602 <123576807+SergioPerez1602@users.noreply.github.com>
2023-03-02 20:44:24 +0100

    Revert "Mejoras"
    This reverts commit 2059f068d724b9b59e0b81046ef66e8ac8e29bbd, reversing
    changes made to ff68c047a252fed076e945fae8315e96198f1649.

--- EndlessRedemption/Assets/Scripts/Player/AttackComponent.cs
+++ EndlessRedemption/Assets/Scripts/Player/AttackComponent.cs
@@ -20,17 +20,14 @@ public class AttackComponent : MonoBehaviour
 
     public void UpAttack()
     {
-        if(!_onUpAttack)
-        {
-            _onUpAttack = true;
-            _animator.SetBool("UpAttack", _onUpAttack);
-            GameObject item = Instantiate(_katana, _myUpTransform.position, Quaternion.identity);
-            item.transform.parent = gameObject.transform;
-        }        
+        _onUpAttack = true;
+        _animator.SetBool("UpAttack", _onUpAttack);
+        GameObject item = Instantiate(_katana, _myUpTransform.position, Quaternion.identity);
+        item.transform.parent = gameObject.transform;
     }
     public void DownAttack()
     {
-        if(!_movementComponent._onGround && !_onDownAttack)
+        if(!_movementComponent._onGround)
         {
             _onDownAttack = true;
             _animator.SetBool("DownAttack", _onDownAttack);
@@ -42,14 +39,10 @@ public class AttackComponent : MonoBehaviour
 
     public void HorizontalAttack()
     {
-        if(!_onAttack)
-        {
-            _onAttack = true;
-            _animator.SetBool("Attack", _onAttack);
-            GameObject item = Instantiate(_katana, _myTransform.position, Quaternion.identity);
-            item.transform.parent = gameObject.transform;
-        }
-      
+        _onAttack = true;
+        _animator.SetBool("Attack", _onAttack);
+        GameObject item= Instantiate(_katana, _myTransform.position,Quaternion.identity);
+        item.transform.parent = gameObject.transform;
         
     }
     public void EndOfAttack()
@@ -72,5 +65,8 @@ public class AttackComponent : MonoBehaviour
     }
 
     // Update is called once per frame
-   
+    void Update()
+    {
+        
+    }
 }

8c98e3eb7fc023970cc273a440e2e084ee7f516b
Averfer <niceghoul52@gmail.com>
2023-03-03 12:56:13 +0100

    Prototipo de araña

---
+++ EndlessRedemption/Assets/Scripts/Enemigos/SpiderBehaviour.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderBehaviour : MonoBehaviour
+{
+    private Transform _playerTransform;
+    [SerializeField]
+    private float _detectionDistance = 0;
+    private Rigidbody2D _myRigidbody;
+    private bool _onGround = false;
+    private BoxCollider2D _myBoxcollider;
+    private Vector2 _vectorDirection;
+    [SerializeField]
+    private float _enemySpeed = 1f;
+    private EnemyManager _enemyManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerTransform = PlayerManager.Instance.transform;
+        _myRigidbody = GetComponent<Rigidbody2D>();
+        _myRigidbody.gravityScale = 0;
+        _myBoxcollider = GetComponent<BoxCollider2D>();
+        _myBoxcollider.enabled = false;
+        _enemyManager = GetComponent<EnemyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _vectorDirection = _playerTransform.position - transform.position;
+        _vectorDirection.Normalize();
+        if (Mathf.Abs(_playerTransform.position.x - transform.position.x) < _detectionDistance && _myRigidbody.gravityScale == 0)
+        {
+            _myRigidbody.gravityScale = 1;
+            _myBoxcollider.enabled = true;
+            transform.localScale = new Vector2(transform.localScale.x, -transform.localScale.y);
+        }
+        else if (_myRigidbody.gravityScale == 1 && _onGround == true)
+        {
+            transform.Translate(_vectorDirection * Time.fixedDeltaTime * _enemySpeed);
+            if (_vectorDirection.x > 0 && transform.localScale.x > 0)
+            {
+
+                _enemyManager.Girar();
+
+            }
+            if (_vectorDirection.x < 0 && transform.localScale.x < 0)
+            {
+
+                _enemyManager.Girar();
+
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null)
+        {
+            _onGround = true;
+        }
+    }
+}

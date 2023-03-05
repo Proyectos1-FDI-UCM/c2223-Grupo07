68dfae33423f3b3fa7a584a6557e3eb9b1f38641
DavidRubioMoreno <darubi04@ucm.es>
2023-03-05 17:56:35 +0100

    Tilemap cueva acabado

--- EndlessRedemption/Assets/Scripts/TransformToParent.cs
+++ EndlessRedemption/Assets/Scripts/TransformToParent.cs
@@ -32,14 +32,5 @@ public class TransformToParent : MonoBehaviour
 
     }
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }

5305df45620c74541f2c6c9e014b7211e07f30ef
javierbravoperucho <jabrav03@ucm.es>
2023-03-06 22:12:10 +0100

    checkpoints + sprites dash y DJ

--- EndlessRedemption/Assets/Scripts/UIManager.cs
+++ EndlessRedemption/Assets/Scripts/UIManager.cs
@@ -17,11 +17,6 @@ public class UIManager : MonoBehaviour
     {
         _instance = this;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()

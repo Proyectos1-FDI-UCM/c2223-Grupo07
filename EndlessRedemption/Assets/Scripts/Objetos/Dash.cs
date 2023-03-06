25ec96e86ad3f56b8ac063b97302e60f98b72c40
javierbravoperucho <jabrav03@ucm.es>
2023-03-06 23:21:49 +0100

    power ups en el hud

--- EndlessRedemption/Assets/Scripts/Objetos/Dash.cs
+++ EndlessRedemption/Assets/Scripts/Objetos/Dash.cs
@@ -10,6 +10,7 @@ public class Dash : MonoBehaviour
         if (collider.gameObject.GetComponent<PlayerManager>())//Si lo coge el jugador solo, para que no triggeree con elementos de la escena
         {
             collider.gameObject.GetComponent<MovementComponent>()._dashPickUp = true;
+            UIManager.Instance.ShowDashIcon();
             Destroy(gameObject);
             Debug.Log("Dash recogido");
         }
--- EndlessRedemption/Assets/Scripts/UIManager.cs
+++ EndlessRedemption/Assets/Scripts/UIManager.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] Vidas = new GameObject[5];
+    private GameObject[] Vidas = new GameObject[5];//Imagenes de vidas
     private int _vidas;
     static private UIManager _instance;
     static public UIManager Instance { get { return _instance; } }
     [SerializeField]
     private TMP_Text shurikenText;
+    [SerializeField]
+    private GameObject DashIcon;
+    [SerializeField]
+    private GameObject DoubleJumpIcon;
 
     private void Awake()
     {
@@ -22,6 +26,16 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         shurikenText.SetText("x" + GameManager.Instance._currenShurikens);
+    }
+
+    public void ShowDashIcon()
+    {
+        DashIcon.SetActive(true);
+    }
+
+    public void ShowDoubleJumpIcon()
+    {
+        DoubleJumpIcon.SetActive(true);
     }
 
     public void GanaVidas()

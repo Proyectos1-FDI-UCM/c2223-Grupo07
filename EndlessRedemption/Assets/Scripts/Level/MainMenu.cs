c6a6748bf5b7aaa66e1c5738f64cfb00757b471c
DavidRubioMoreno <darubi04@ucm.es>
2023-03-13 16:19:50 +0100

    mejora ambientacion cueva

--- EndlessRedemption/Assets/Scripts/Level/MainMenu.cs
+++ EndlessRedemption/Assets/Scripts/Level/MainMenu.cs
@@ -12,9 +12,11 @@ public class MainMenu : MonoBehaviour
 
     public void PlayNightmareGame()
     {
+       
         SceneManager.LoadScene("Level1"); //Cargar juego en modo pesadilla
         GameManager.Instance._maxLifes = 3;
         GameManager.Instance._maxShurikens = 10;
+
     }
 
     public void QuitGame()

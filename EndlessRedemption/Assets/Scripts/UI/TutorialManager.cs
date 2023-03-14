bdd474be289887f121516e1434058796c48c7c34
javierbravoperucho <jabrav03@ucm.es>
2023-03-14 22:44:41 +0100

    tutorial

--- EndlessRedemption/Assets/Scripts/UI/TutorialManager.cs
+++ EndlessRedemption/Assets/Scripts/UI/TutorialManager.cs
@@ -10,26 +10,19 @@ public class TutorialManager : MonoBehaviour
     [SerializeField]
     private GameObject[] _tutorialBoxes = new GameObject[4];//array de cajas de texto
     private float _elapsedTime = 0f;
-    private float _boxTime = 2f;
+    private float _boxTime = 1f;
+    private bool _boxClicked = false;
+    private int i = 0; //indice del array
 
-    private void ClickBox(int i)
+    public void ClickBox()
     {
-        _tutorialBoxes[i].SetActive(false);
-        if (i < 3)
-        {
-            while(_elapsedTime< _boxTime)
-            {
-                _elapsedTime += Time.deltaTime;
-            }
-            _tutorialBoxes[i+1].SetActive(true);
-            _elapsedTime= 0f;
-        }
+        _boxClicked = true;
+        Debug.Log("Click");
     }
 
     private void OnTriggerEnter2D(Collider2D collider2D)//Trigger puesto en el primer checkpoint
     {
         _tutorial.SetActive(true);
-
     }
 
     // Start is called before the first frame update
@@ -41,6 +34,20 @@ public class TutorialManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_boxClicked)
+        {
+            _tutorialBoxes[i].SetActive(false);
+            _elapsedTime += Time.deltaTime;
+            if(_elapsedTime > _boxTime)
+            {
+                if (i < 3)
+                {
+                    _tutorialBoxes[i + 1].SetActive(true);
+                    _elapsedTime = 0f;
+                    i++;
+                    _boxClicked = false;
+                }
+            }  
+        }
     }
 }

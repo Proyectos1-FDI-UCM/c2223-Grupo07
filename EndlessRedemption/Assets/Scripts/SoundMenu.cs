ed1683bd2dfd9c3bc34abdae1be952a15e48d147
DavidRubioMoreno <darubi04@ucm.es>
2023-03-16 08:18:17 +0100

    arreglo pinchos cueva + intro menu mus

--- EndlessRedemption/Assets/Scripts/SoundMenu.cs
+++ EndlessRedemption/Assets/Scripts/SoundMenu.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class SoundMenu : MonoBehaviour
 {
+    [SerializeField]
     private AudioClip[] _audioClips;
+    private AudioSource _audioSourceMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _audioSourceMenu = GetComponent<AudioSource>();
+        PlaySound(0, 0.2f);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
     private void PlaySound(int pista, float volume)
     {
-      
+        _audioSourceMenu.PlayOneShot(_audioClips[pista], volume);
     }
 }

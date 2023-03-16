01863e8de68956f2598634ea9199bf27e664ba9f
Alvaro <alvarver@ucm.es>
2023-03-16 17:43:22 +0100

    Cambio de escena con fundido a negro

--- EndlessRedemption/Assets/Scripts/Level/FinishLevel1.cs
+++ EndlessRedemption/Assets/Scripts/Level/FinishLevel1.cs
@@ -7,10 +7,26 @@ public class FinishLevel1 : MonoBehaviour
 {
     [SerializeField]
     private int _nextLevel;
+
+    [SerializeField]
+    private float _transitionTime = 1f;
+    private Animator _transitionAnimator;
+
+    private void Start()
+    {
+        _transitionAnimator = GetComponentInChildren<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerPrefs.SetInt("LevelX", _nextLevel);
         PlayerPrefs.SetInt("CheckpointX", 0);
-        SceneManager.LoadScene(_nextLevel+1);
+        StartCoroutine(SceneLoad(_nextLevel));
+    }
+
+    private IEnumerator SceneLoad(int _nextLevel)
+    {
+        _transitionAnimator.SetTrigger("StartTransition");
+        yield return new WaitForSeconds(_transitionTime);
+        SceneManager.LoadScene(_nextLevel + 1);
     }
 }

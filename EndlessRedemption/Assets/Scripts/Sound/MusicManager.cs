b192fc305aae298238a0bbe0772c763be053a6f8
DavidRubioMoreno <darubi04@ucm.es>
2023-05-12 17:30:15 +0200

    music cambio

--- EndlessRedemption/Assets/Scripts/Sound/MusicManager.cs
+++ EndlessRedemption/Assets/Scripts/Sound/MusicManager.cs
@@ -42,6 +42,7 @@ public class MusicManager : MonoBehaviour
                     SelectSong(1, 0.1f);
                     break;
                 case 4:
+                    SelectSong(0, 0.3f);
                     SelectSong(1, 0.3f);
                     break;
                 default:
@@ -64,11 +65,11 @@ public class MusicManager : MonoBehaviour
                     SelectSong(0, 0.1f);
                     SelectSong(1, 3f);
                     break;
-                case 3:
-                    SelectSong(0, 0.3f);
+                case 3:                
                     SelectSong(1, 0.1f);
                     break;
                 case 4:
+                    SelectSong(0, 0.3f);
                     SelectSong(1, 0.3f);
                     break;
             }
